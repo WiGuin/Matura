356730397a93718d273d034b3a2839370c0a8019
Leandro Guido <guido.leandro@bluewin.ch>
2023-03-09 22:37:03 +0100

    placing surface in rect and then rect on screen + collision

--- Üben & Ausprobieren Lele/Ausprobieren.py
+++ Üben & Ausprobieren Lele/Ausprobieren.py
@@ -13,8 +13,10 @@ ground_surface = pygame.image.load(os.path.dirname(__file__)+s+'Graphics/ground.
 text_surface = test_font.render('My Game', False, 'Green')
 
 snail_surface = pygame.image.load(os.path.dirname(__file__)+s+'Graphics/snail/snail1.png'). convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(bottomright = (600, 300))
 
+player_surface = pygame.image.load(os.path.dirname(__file__)+s+'Graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80, 300))
 
 while True:
 
@@ -26,9 +28,13 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface, (325, 50))
-    screen.blit(snail_surface, (snail_x_pos, 250))
-    snail_x_pos = snail_x_pos - 4
-    if snail_x_pos < -50:
-        snail_x_pos = 800
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
+    snail_rect.right -= 4
+    if snail_rect.right <= 0:
+        snail_rect.right = 800
+    
+    if player_rect.colliderect(snail_rect):
+        print('collision')
     pygame.display.flip()
     clock.tick(60)
