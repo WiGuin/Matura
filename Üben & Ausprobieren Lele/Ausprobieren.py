945ec57e7d0a13de0ca074ddd7e7600ef65547fa
Salvatore Guido <guido.leandro@bluewin.ch>
2023-03-12 21:42:40 +0100

    weiter arbeiten

--- Üben & Ausprobieren Lele/Ausprobieren.py
+++ Üben & Ausprobieren Lele/Ausprobieren.py
@@ -10,7 +10,9 @@ test_font = pygame.font.Font(os.path.dirname(__file__)+s+'Font/Pixeltype.ttf', 5
 
 sky_surface = pygame.image.load(os.path.dirname(__file__)+s+'Graphics/sky.png').convert()
 ground_surface = pygame.image.load(os.path.dirname(__file__)+s+'Graphics/ground.png').convert()
-text_surface = test_font.render('My Game', False, 'Green')
+
+score_surface = test_font.render('My Game', False, (64,64,64))
+score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load(os.path.dirname(__file__)+s+'Graphics/snail/snail1.png'). convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (600, 300))
@@ -24,17 +26,25 @@ while True:
 
         if event.type == QUIT:
             pygame.quit()
+        
+        #if event.type == pygame.MOUSEMOTION:
+        #    if player_rect.collidepoint(event.pos):
+        #        print('hit')
     
+
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (325, 50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect,)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+    screen.blit(score_surface, score_rect)
     screen.blit(snail_surface, snail_rect)
     screen.blit(player_surface, player_rect)
     snail_rect.right -= 4
     if snail_rect.right <= 0:
         snail_rect.right = 800
     
-    if player_rect.colliderect(snail_rect):
-        print('collision')
+    #if player_rect.colliderect(snail_rect):
+     #   print('collision')
+
     pygame.display.flip()
     clock.tick(60)
