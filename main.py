6297be86fefa627efbcb8c5c6bf747c3cd8c089b
Alexander Dinacher <alexander@dinacher.com>
2023-05-09 19:40:18 +0200

    First part of movement

--- main.py
+++ main.py
@@ -15,9 +15,27 @@ class Background(pygame.sprite.Sprite):
     def print(self):
         screen.blit(self.surf, (self.x,self.y))
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        self.surf=pygame.image.load(os.path.dirname(__file__)+s+"textures"+s+"Characters"+s+"Character 1_1.2 ohne Items.png")
+        self.surf=pygame.transform.scale(self.surf, (125,125))
 
+    def print(self):
+        screen.blit(self.surf, (438,438))
+
+    def walk(self, key):
+        if key=="w":
+            background.y+=10
+        if key=="s":
+            background.y-=10
+        if key=="a":
+            background.x+=10
+        if key=="d":
+            background.x-=10
+        #print(str(background.x) + " and " + str(background.y))
 
 background = Background()
+player = Player()
 
 menu = False
 gameon = True
@@ -25,12 +43,23 @@ gameon = True
 while gameon:
     
     background.print()
+    player.print()
 
     for event in pygame.event.get():
 
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 menu=True
+
+            if event.key == K_w:
+                player.walk("w")
+            if event.key == K_s:
+                player.walk("s")
+            if event.key == K_a:
+                player.walk("a")
+            if event.key == K_d:
+                player.walk("d")
+
         elif event.type == QUIT:
             gameon=False
 
